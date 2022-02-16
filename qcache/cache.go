bf23c18cdc975fd847f1f1c2b00f8a4d2c63b6a8
qizong007 <978451432@qq.com>
2022-02-16 21:34:40 +0800

    fix: cache.go pkg

--- qcache/cache.go
+++ qcache/cache.go
@@ -1,14 +1,14 @@
 package qcache
 
 import (
-	lru2 "QCache/qcache/lru"
+	"QCache/qcache/lru"
 	"sync"
 )
 
 // 对 lru 的进一步封装，主要是做并发控制
 type cache struct {
 	mu         sync.Mutex
-	lru        *lru2.Cache
+	lru        *lru.Cache
 	cacheBytes int64
 }
 
@@ -16,7 +16,7 @@ func (c *cache) set(key string, value ByteView) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	if c.lru == nil {
-		c.lru = lru2.New(c.cacheBytes, nil) // Lazy Initialization
+		c.lru = lru.New(c.cacheBytes, nil) // Lazy Initialization
 	}
 	c.lru.Set(key, value)
 }
